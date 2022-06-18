0b99806da4c97d23e34d08b3c4ee456897afcd85
hjh <1556332597@qq.com>
2022-06-18 15:53:07 +0800

    推了

--- pages/voice-detail/voice-detail.js
+++ pages/voice-detail/voice-detail.js
@@ -8,6 +8,13 @@ Page({
    * 页面的初始数据
    */
   data: {
+    content:"",
+    content2:"",
+    curr: 0,
+    currgroup:"",
+    show: false,
+    show2: false,
+    show3: false,
     sentence:[],
     array:[],
     sentencedetail:[],
@@ -65,20 +72,81 @@ Page({
       }
     })
   },
-  onClose(event) {
-    const { position, instance } = event.detail;
-    switch (position) {
-      case 'left':
-      case 'cell':
-        instance.close();
-        break;
-      case 'right':
-        Dialog.confirm({
-          message: '确定删除吗？',
-        }).then(() => {
-          instance.close();
-        });
-        break;
-    }
+  onDelete(item) {
+    //const { position, instance } = event.detail;
+    var s = item.currentTarget.dataset.item.id
+    this.setData({
+      show: true,
+      curr: s
+    })
   },
+  onChange(event) {
+    // event.detail 为当前输入的值
+    this.setData({
+      content:event.detail
+    })
+  },
+  onChange2(event) {
+    // event.detail 为当前输入的值
+    this.setData({
+      content2:event.detail
+    })
+  },
+  onEdit(item) {
+    //const { position, instance } = event.detail;
+    var s = item.currentTarget.dataset.item.id
+    this.setData({
+      show2: true,
+      curr: s
+    })
+  },
+  onAdd(item) {
+    //const { position, instance } = event.detail;
+    //console.log(item)
+    var s = item.currentTarget.dataset.item
+    this.setData({
+      show3: true,
+      currgroup:s
+    })
+  },
+  onClose(item) {
+    //const { position, instance } = event.detail;
+    this.setData({
+      show: false,
+      show2:false,
+      show3:false
+    })
+  },
+  delete() {
+    console.log(this.data.curr)
+    wx.request({
+      url: 'https://bewcf.info/sentence/removeSentence?sentenceId='+this.data.curr,
+      method:"POST",
+      success:(res)=>{
+        console.log("删除成功")
+        this.onLoad();
+      }
+    })
+  },
+  edit() {
+    wx.request({
+      url: 'https://bewcf.info/sentence/updateSentence?sentenceId='+this.data.curr+'&content='+this.data.content,
+      method:'POST',
+      success:(res)=>{
+        console.log("修改成功")
+        this.onLoad();
+      }
+    })
+  },
+  add() {
+    console.log(this.data.currgroup);
+    wx.request({
+      url: 'https://bewcf.info/sentence/addSentence?userId='+this.data.userId+'&name='+this.data.currgroup+'&content='+this.data.content2,
+      method:'POST',
+      success:(res)=>{
+        console.log("修改成功")
+        this.onLoad();
+      }
+    })
+  } 
 })
